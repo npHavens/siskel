b19d58545bb4fe13c2954067e3c37baf980e8f82
Nick Havens <npahavens@gmail.com>
2017-06-27 10:38:59 -0500

    added code for Movies collection model tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-   this.set('like', !this.get('like'))
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,13 +15,14 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
